cd5f76f14f03bff3e59559f18626b09c6d3af175
Marcus Wetterbro <wetterbro@gmail.com>
2024-09-18 10:53:35 +0200

    added WhoAmI

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -145,5 +145,24 @@ namespace API.Controllers
                 return StatusCode(500, new { Error = "An unexpected error occurred." });
             }
         }
+        [Authorize]
+        [HttpGet("WhoAmI")]
+        public async Task<IActionResult> WhoAmI()
+        {
+            try
+            {
+                var isAdmin = User.IsInRole("Admin");
+                var user = await _userManager.GetUserAsync(User);
+                return Ok(new { user.Id, user.AccountName, isAdmin });
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "An unexpected error occurred.");
+            }
+        }
+        
+        
+        
+        
     }
 }
